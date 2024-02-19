76449f4cac5162f6803d521b67af298926cabe13
benwalsh23 <walshb23@mymacewan.ca>
2024-02-18 20:53:46 -0700

    hopefully this works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+
 import AvailableCourses from './AvailableCourses';
 import RegisteredCourses from './RegisteredCourses';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Route exact path="/" component={AvailableCourses} />
-        <Route path="/registered-courses" component={RegisteredCourses} />
-      </div>
+      <Routes> 
+        <Route exact path="/" element={<AvailableCourses />} /> 
+        <Route path="/registered-courses" element={<RegisteredCourses />} /> 
+      </Routes>
     </Router>
   );
 }
